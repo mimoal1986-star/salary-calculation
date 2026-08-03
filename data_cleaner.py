e04ed8f5a40a2ff75e69a59e705b202d22b6f8b2
MikMosk86 <mimoal1986@mail.ru>
2026-08-03 15:28:56 +0300

    Update data_cleaner.py

--- data_cleaner.py
+++ data_cleaner.py
@@ -268,8 +268,8 @@ def fill_rs_and_em(cleaned_df, projects_df, name_login_df):
             rs_value = str(row['RS']).strip()
             login_rs = str(row['Логин RS']).strip()
             
-            # Проверяем, что RS пустая или содержит - / 0 / koordinator
-            if rs_value == '' or rs_value == '-' or rs_value == '0' or 'koordinator' in rs_value.lower():
+            # Проверяем, что RS пустая или содержит - / 0 / koordinator / rukovoditel
+            if rs_value == '' or rs_value == '-' or rs_value == '0' or 'koordinator' in rs_value.lower() or 'rukovoditel' in rs_value.lower():
                 # Ищем в словаре по логин rs
                 if login_rs in login_to_name:
                     return login_to_name[login_rs]
